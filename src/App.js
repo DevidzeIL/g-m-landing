5440a61cec99b7612bf1f8f3ac6f936a7afa1f71
DevidzeIL <devidzeil@icloud.com>
2023-03-21 22:46:12 +0400

    add Portfolio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import Technologies from './components/Technologies';
-import Hero from './components/Hero';
-import About from './components/About';
-import Footer from './components/Footer';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import About from './components/About';
+import Team from './components/Team';
+import Portfolio from './components/Portfolio';
+import Technologies from './components/Technologies';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className='app'>
       <Header />
       <Hero />
+      <Services />
       <About />
+      <Team />
+      <Portfolio />
       <Technologies />
       <Footer />
     </div>
