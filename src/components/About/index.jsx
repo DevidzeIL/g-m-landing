de8ae30e8caa3b5687a574069546b6ba87e254bc
DevidzeIL <devidzeil@icloud.com>
2023-03-21 21:14:04 +0400

    edit About

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,15 +1,14 @@
-import "./About.css";
+import './About.css';
 
 export default function About() {
   return (
-    <div className="page-about">
+    <div className='page-about'>
       <h1>About us</h1>
       <h3>
-        We are a team of friends who share a passion for developing. Our company
-        specializes in developing{" "}
-        <span style={{ color: "black" }}> web and mobile applications.</span>
-        With our diverse set of skills and experience, we strive to deliver
-        high-quality products that meet the unique needs of our clients.
+        We are a team of friends who share a passion for developing. Our company specializes in
+        developing <span style={{ color: 'black' }}> web and mobile applications. </span>
+        With our diverse set of skills and experience, we strive to deliver high-quality products
+        that meet the unique needs of our clients.
       </h3>
     </div>
   );
