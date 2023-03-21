a9500b6e24e21dc787b636fdb62568e4ff2f4020
DevidzeIL <devidzeil@icloud.com>
2023-03-21 22:45:58 +0400

    update Team. Add hover to blocks

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,12 +1,44 @@
+import { useState } from 'react';
 import './Team.css';
+import github from '../../assets/icons/github.svg';
+import linkedin from '../../assets/icons/linkedin.svg';
 
 function Member({ members }) {
+  const [hoveredMemberIndex, setHoveredMemberIndex] = useState(-1);
+
   return (
     <>
       {members.map((item, index) => (
-        <div className='member' key={index}>
-          <div className='member-name'>{item.name}</div>
-          <div className='member-role'>{item.role}</div>
+        <div
+          className={`member ${hoveredMemberIndex === index ? 'hovered' : ''}`}
+          key={index}
+          onMouseEnter={() => setHoveredMemberIndex(index)}
+          onMouseLeave={() => setHoveredMemberIndex(-1)}
+        >
+          {hoveredMemberIndex === index ? (
+            <>
+              <div className='member-header'>
+                <div className='member-name'>{item.name}</div>
+                <div className='member-role'>{item.info}</div>
+              </div>
+              <div className='member-links'>
+                {item.links.map((link, index) => (
+                  <>
+                    <div key={index}>
+                      <a href={link.link}>
+                        <img src={link.icon} alt={link.name} />
+                      </a>
+                    </div>
+                  </>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='member-name'>{item.name}</div>
+              <div className='member-role'>{item.role}</div>
+            </>
+          )}
         </div>
       ))}
     </>
@@ -14,12 +46,60 @@ function Member({ members }) {
 }
 
 const members = [
-  { name: 'Mary Sargsyan', role: 'CEO / Full-stack developer /Designer' },
-  { name: 'Iliya Devidze', role: 'Full-stack developer Designer' },
-  { name: 'Aleksandr Zakalsky', role: 'Full-stack developer' },
-  { name: 'Vlad Gladky', role: 'Mobile developer' },
-  { name: 'Ivan Timoshov', role: 'Mobile developer' },
-  { name: 'Nikita Bondarev', role: 'Mobile developer' },
+  {
+    name: 'Mary Sargsyan',
+    role: 'CEO / Full-stack developer /Designer',
+    info: 'info',
+    links: [
+      { name: 'github', link: 'https://github.com/', icon: github },
+      { name: 'linkedin', link: 'https://www.linkedin.com/', icon: linkedin },
+    ],
+  },
+  {
+    name: 'Iliya Devidze',
+    role: 'Full-stack developer Designer',
+    info: 'info',
+    links: [
+      { name: 'github', link: 'https://github.com/', icon: github },
+      { name: 'linkedin', link: 'https://www.linkedin.com/', icon: linkedin },
+    ],
+  },
+  {
+    name: 'Aleksandr Zakalsky',
+    role: 'Full-stack developer',
+    info: 'info',
+    links: [
+      { name: 'github', link: 'https://github.com/', icon: github },
+      { name: 'linkedin', link: 'https://www.linkedin.com/', icon: linkedin },
+    ],
+  },
+  {
+    name: 'Vlad Gladky',
+    role: 'Mobile developer',
+    info: 'info',
+    links: [
+      { name: 'github', link: 'https://github.com/', icon: github },
+      { name: 'linkedin', link: 'https://www.linkedin.com/', icon: linkedin },
+    ],
+  },
+  {
+    name: 'Ivan Timoshov',
+    role: 'Mobile developer',
+    info: 'info',
+    links: [
+      { name: 'github', link: 'https://github.com/', icon: github },
+      { name: 'linkedin', link: 'https://www.linkedin.com/', icon: linkedin },
+    ],
+  },
+  {
+    name: 'Nikita Bondarev',
+    role: 'Mobile developer',
+    info: 'info',
+    links: [
+      { name: 'github', link: 'https://github.com/', icon: github },
+      { name: 'linkedin', link: 'https://www.linkedin.com/', icon: linkedin },
+    ],
+  },
 ];
 
 export default function Team() {
